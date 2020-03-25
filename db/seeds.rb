b7a1b6f2d2da95e815124a52b936abadfb4354f9
parsley <topthecap@gmail.com>
2020-03-25 09:55:38 +0000

    seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require 'csv'
 
-CSV.foreach('db/data_5.csv', headers: true) do |row|
+CSV.foreach('db/data_6.csv', headers: true) do |row|
   Post.create(
     name: row['name'],
     byname: row['byname'],
